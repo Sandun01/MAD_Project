3bc3ca045ed41c24835cdd9bb1c40f0d0037ac34
Nethmi <amaranayake.nethmi@gmail.com>
2020-09-24 12:57:44 +0530

    delete details function created - nethmi

--- app/src/main/java/com/example/mad/woofadmin_org_details.java
+++ app/src/main/java/com/example/mad/woofadmin_org_details.java
@@ -4,21 +4,76 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class woofadmin_org_details extends AppCompatActivity {
 
-    Button button;
+    EditText txtDelClinic, txtDelConNo, txtDelAddress, txtDelCity, txtDelDescription, txtDelOwner;
+    Button buttonDelete;
+    DatabaseReference dbRef;
+    DogCare deleteClinic;
+
+    private void clearControls(){
+        txtDelClinic.setText("");
+        txtDelConNo.setText("");
+        txtDelAddress.setText("");
+        txtDelCity.setText("");
+        txtDelDescription.setText("");
+        txtDelOwner.setText("");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_woofadmin_org_details);
 
-        button = findViewById(R.id.woofadmin_org_update);
+        txtDelClinic = findViewById(R.id.deleteClinic);
+        txtDelConNo = findViewById(R.id.deleteConNo);
+        txtDelAddress = findViewById(R.id.deleteAddress);
+        txtDelCity = findViewById(R.id.deleteCity);
+        txtDelDescription = findViewById(R.id.deleteDescription);
+        txtDelOwner = findViewById(R.id.deleteOwner);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        buttonDelete = findViewById(R.id.woofadmin_org_update);
+
+        deleteClinic = new DogCare();
+
+        buttonDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatabaseReference delRef = FirebaseDatabase.getInstance().getReference().child("deleteClinic");
+                delRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (snapshot.hasChild("deleteClinic1")){
+                            dbRef = FirebaseDatabase.getInstance().getReference().child("DogCare").child("deleteClinic1");
+                            dbRef.removeValue();
+                            clearControls();
+                            Toast.makeText(getApplicationContext(), "Data Deleted Successfully...", Toast.LENGTH_SHORT).show();
+                        }
+                        else
+                            Toast.makeText(getApplicationContext(), "No source to Delete...", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+            }
+        });
+
+        buttonDelete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(woofadmin_org_details.this,woofadmin_org_update.class);
--- app/src/main/java/com/example/mad/woofadmin_org_update.java
+++ app/src/main/java/com/example/mad/woofadmin_org_update.java
@@ -1,5 +1,6 @@
 package com.example.mad;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
