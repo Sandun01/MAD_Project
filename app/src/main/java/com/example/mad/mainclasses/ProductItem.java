7a4953915bde5b6bbb610212f8a642ac0d7c39d2
Sandun01 <sandunlakshitha06@gmail.com>
2020-09-24 22:23:41 +0530

    completed recycler view1 - Sandun

---
+++ app/src/main/java/com/example/mad/mainclasses/ProductItem.java
@@ -0,0 +1,37 @@
+package com.example.mad.mainclasses;
+
+
+
+public class ProductItem {
+
+    private String ProductName;
+    private String Description;
+    private Float UnitPrice;
+
+    public ProductItem() {
+    }
+
+    public String getProductName() {
+        return ProductName;
+    }
+
+    public void setProductName(String productName) {
+        ProductName = productName;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public Float getUnitPrice() {
+        return UnitPrice;
+    }
+
+    public void setUnitPrice(Float unitPrice) {
+        UnitPrice = unitPrice;
+    }
+}
