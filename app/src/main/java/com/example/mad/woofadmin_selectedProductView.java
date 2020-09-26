efcf862848f743d5c0376b89061b2bfaf3632dd6
Ravija <ravijaloveshiphop@gmail.com>
2020-09-26 23:48:10 +0530

    ravija delete/update product created

--- app/src/main/java/com/example/mad/woofadmin_selectedProductView.java
+++ app/src/main/java/com/example/mad/woofadmin_selectedProductView.java
@@ -1,9 +1,12 @@
 package com.example.mad;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -65,6 +68,41 @@ public class woofadmin_selectedProductView extends AppCompatActivity {
 
             }
         });
+
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dbRef=FirebaseDatabase.getInstance().getReference().child("ProductItem").child(itemID);
+                dbRef.removeValue();
+                Toast.makeText(getApplicationContext(),"Product deleted successfully",Toast.LENGTH_SHORT).show();
+
+
+                navigateToViewProd();
+            }
+
+
+        });
+
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dbRef=FirebaseDatabase.getInstance().getReference();
+                dbRef.child("ProductItem").child(itemID).child("productName").setValue(name.getText().toString().trim());
+                dbRef.child("ProductItem").child(itemID).child("description").setValue(desc.getText().toString().trim());
+                dbRef.child("ProductItem").child(itemID).child("qty").setValue(qty.getText().toString().trim());
+                dbRef.child("ProductItem").child(itemID).child("unitPrice").setValue(price.getText().toString().trim());
+
+                Toast.makeText(getApplicationContext(),"Item Updated successfully",Toast.LENGTH_SHORT).show();
+                navigateToViewProd();
+
+            }
+        });
+
+        }
+    public void navigateToViewProd(){
+        Intent intent = new Intent(this, woofadmin_viewProduct.class);
+        startActivity(intent);
+
 
 
 
