f7bd53683420e0ed242be90213c49653f98a32a5
Ravija <ravijaloveshiphop@gmail.com>
2020-09-26 10:47:11 +0530

    ravija recyclerview created

--- app/src/main/java/com/example/mad/woofadmin_selectedProductView.java
+++ app/src/main/java/com/example/mad/woofadmin_selectedProductView.java
@@ -6,9 +6,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class woofadmin_selectedProductView extends AppCompatActivity {
 
+    String itemID;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_woofadmin_selected_product_view);
+
+        itemID =  getIntent().getStringExtra("itmID");
+        System.out.println("----------------------------"+itemID);
+
     }
 }
--- app/src/main/java/com/example/mad/woofadmin_viewProduct.java
+++ app/src/main/java/com/example/mad/woofadmin_viewProduct.java
@@ -3,7 +3,9 @@ package com.example.mad;
 import android.content.Intent;
 import android.media.MediaMetadataRetriever;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -13,27 +15,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.mad.viewholders.ProductViewholder;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class woofadmin_viewProduct extends AppCompatActivity {
 
-    FirebaseStorage mStorage;
-    FirebaseDatabase mDatabase;
-    DatabaseReference dbRef;
-    RecyclerView recyclerView;
-    ProductItem_Adapter productItem_adapter;
-    List<ProductItem>productItemList;
 
     TextView toViewProduct;
     Button addItem;
+    private DatabaseReference prRef;
+    private RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,57 +46,72 @@ public class woofadmin_viewProduct extends AppCompatActivity {
 
         addItem = findViewById(R.id.btnAdmin_addItem);
 
-//        mDatabase = FirebaseDatabase.getInstance();
-//        dbRef = FirebaseDatabase.getInstance().getReference().child("ProductItem");
-//        mStorage = FirebaseStorage.getInstance();
-//        recyclerView = findViewById(R.id.recyclerView_Products);
-//        recyclerView.setHasFixedSize(true);
-//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+                //initializing
+        prRef = FirebaseDatabase.getInstance().getReference().child("ProductItem");
 
-//        productItemList=new ArrayList<ProductItem>();
-//        productItem_adapter =new ProductItem_Adapter(woofadmin_viewProduct.this,productItemList);
-//        recyclerView.setAdapter(productItem_adapter);
+        recyclerView = findViewById(R.id.recyclerView_Products);
+        recyclerView.setHasFixedSize(true);
 
-//        dbRef.addChildEventListener(new ChildEventListener() {
-//            @Override
-//            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-//                ProductItem productItem=snapshot.getValue(ProductItem.class);
-//                productItemList.add(productItem);
-//                productItem_adapter.notifyDataSetChanged();
-//            }
-//
-//            @Override
-//            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-//
-//            }
-//
-//            @Override
-//            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
-//
-//            }
-//
-//            @Override
-//            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-//
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError error) {
-//
-//            }
-//        });
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerOptions<ProductItem> options =
+                new FirebaseRecyclerOptions.Builder<ProductItem>()
+                        .setQuery(prRef, ProductItem.class)
+                        .build();
+
+        FirebaseRecyclerAdapter<ProductItem, ProductViewholder> adapter =
+                new FirebaseRecyclerAdapter<ProductItem, ProductViewholder>(options) {
+
+                    @Override
+                    protected void onBindViewHolder(@NonNull ProductViewholder holder, int i, @NonNull final ProductItem model)
+                    {
+                        holder.pnameTxt.setText(model.getProductName());
+                        holder.priceTxt.setText("Price: Rs." + model.getUnitPrice().toString());
+                        holder.pdesTxt.setText("Description: " + model.getDescription());
+                        Picasso.get().load(model.getImage()).into(holder.imageView);
+
+                        holder.itemView.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Intent intent = new Intent(woofadmin_viewProduct.this, woofadmin_selectedProductView.class);
+                                intent.putExtra("itmID", model.getId());
+                                startActivity(intent);
+
+                            }
+                        });
+
+                    }
+
+                    @NonNull
+                    @Override
+                    public ProductViewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+                    {
+
+                        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.design_rows_for_rv, parent,false);
+
+                        ProductViewholder holder = new ProductViewholder(view);
+
+                        return holder;
+                    }
+                };
+
+        recyclerView.setAdapter(adapter);
+        adapter.startListening();
+
+    }
+
 
     @Override
     protected void onResume() {
         super.onResume();
-//        toViewProduct.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(woofadmin_viewProduct.this,woofadmin_selectedProductView.class);
-//                startActivity(intent);
-//            }
-//        });
 
         addItem.setOnClickListener(new View.OnClickListener() {
             @Override
